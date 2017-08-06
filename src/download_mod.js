0411d0e9d966ca6b7b21c4e5e2d3c8063716893c
Björn Dahlgren <bjorn@dahlgren.at>
2017-08-06 19:41:30 +0200

    Change path property used by steamcmd module

--- src/download_mod.js
+++ src/download_mod.js
@@ -3,7 +3,7 @@ var steamcmd = require('steamcmd')
 module.exports = function (options, callback) {
   steamcmd.install({
     applicationId: 107410,
-    installPath: options.path,
+    path: options.path,
     password: options.password,
     username: options.username,
     workshopId: options.workshopId,
