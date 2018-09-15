a5be64dc481d6ff90f3afdb2cda38495562f8331
Björn Dahlgren <bjorn@dahlgren.at>
2018-09-15 23:01:41 +0200

    Move ACF logic to separate files

--- test/delete_mod_metadata.test.js
+++ test/delete_mod_metadata.test.js
@@ -1,16 +1,18 @@
 var fs = require('fs')
 var path = require('path')
 var should = require('should')
+var VDF = require('@node-steam/vdf')
+
 var deleteModMetadata = require('../src/delete_mod_metadata')
 
 var workshopFolder = path.join(__dirname, 'data', 'steamapps', 'workshop')
-var testData = fs.readFileSync(path.join(workshopFolder, 'appworkshop_with_cba_a3.acf'), 'utf-8')
-var expected = fs.readFileSync(path.join(workshopFolder, 'appworkshop_empty.acf'), 'utf-8')
+var testData = VDF.parse(fs.readFileSync(path.join(workshopFolder, 'appworkshop_with_cba_a3.acf'), 'utf-8'))
+var expected = VDF.parse(fs.readFileSync(path.join(workshopFolder, 'appworkshop_empty.acf'), 'utf-8'))
 
 describe('delete mod metadata', function () {
   it('should have mod metadata deleted', function () {
     var actual = deleteModMetadata(testData, '450814997')
 
-    should.equal(actual, expected)
+    should.deepEqual(actual, expected)
   })
 })
